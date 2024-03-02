77e0671a67659ab9023f33bac477942678d60e3a
liza <122369834+lizasizas@users.noreply.github.com>
2024-03-03 00:06:43 +0700

    Update dashboard.py
    ubah

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -57,7 +57,7 @@ def create_rfm_df(df):
     
     return rfm_df
 
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("dashboard/all_data.csv")
 
 datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
 all_df.sort_values(by="order_purchase_timestamp", inplace=True)
